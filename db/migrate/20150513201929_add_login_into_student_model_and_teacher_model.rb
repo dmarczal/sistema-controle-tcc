99d2b18c674330491a9f8febcf7b1bcc21aa2f09
erico dias <ericodias1@gmail.com>
2015-05-14 17:40:28 -0300

    #change to drop table IF EXISTS

--- db/migrate/20150513201929_add_login_into_student_model_and_teacher_model.rb
+++ db/migrate/20150513201929_add_login_into_student_model_and_teacher_model.rb
@@ -1,6 +1,5 @@
 class AddLoginIntoStudentModelAndTeacherModel < ActiveRecord::Migration
   def change
-    drop_table :logins, if_exists: true
     add_column :students, :login, :string
     add_column :teachers, :login, :string
     add_column :teachers, :role_id, :integer
