0ddc7d68ae0be5cb022053228c00f1003ed8d767
Érico Dias <ericodias1@gmail.com>
2015-08-17 16:58:29 -0300

    login adjusts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,15 +13,17 @@ class ApplicationController < ActionController::Base
       # TODO Ldap connect
       @user = Student.find_by(login: params[:user][:login])
       session[:resource] = 0
-    else
-      redirect_to login_path, :flash => { :danger => t('controllers.login.user_not_found') }
     end
 
-    if(!@user.password.check?(params[:user][:password]))
-      redirect_to login_path, :flash => { :danger => t('controllers.login.incorrect_password') }
+    if !@user
+      redirect_to login_path, :flash => { :danger => t('controllers.login.user_not_found') }
     else
-      session[:user_id] = @user.id
-      redirect_to get_redirect_path, :flash => { :success => t('controllers.login.success_login') }
+      if(!@user.password.check?(params[:user][:password]))
+        redirect_to login_path, :flash => { :danger => t('controllers.login.incorrect_password') }
+      else
+        session[:user_id] = @user.id
+        redirect_to get_redirect_path, :flash => { :success => t('controllers.login.success_login') }
+      end
     end
   end
 
