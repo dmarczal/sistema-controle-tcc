89d6f798b0b9c77b56d5e65a4f4a77614862eab9
Érico Dias <ericodias1@gmail.com>
2015-08-23 20:18:38 -0300

    ajustes nos seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@
 Role.create(name: 'Professor responsável')
 Role.create(name: 'Professor de TCC 1')
 Role.create(name: 'Professor')
-Teacher.create(name: 'Diego Marczal', lattes: 'http://lates.cnpq.br', atuacao: 'Programação web', email: 'email@email.com', login: 'diego', role: Role.find_by(:name => "Professor responsável"))
+Teacher.create(name: 'Diego Marczal', lattes: 'http://lates.cnpq.br', atuacao: 'Programação web', email: 'ericodias1@gmail.com', login: 'responsavel', role: Role.find_by(:name => "Professor responsável"))
+Teacher.create(name: 'Guilherme Silva', lattes: 'http://lates.cnpq.br', atuacao: 'Programação web', email: 'ericodias1@gmail.com', login: 'tcc1', role: Role.find_by(:name => "Professor de TCC 1"))
 TypeApproval.create(name: "Aprovado")
 TypeApproval.create(name: "Aprovado com restrições")
 StatusItem.create name: "Pendente"
