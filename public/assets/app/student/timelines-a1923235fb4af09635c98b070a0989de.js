624575ce3d15a6c497340de2593590bb99c66584
Érico Dias <ericodias1@gmail.com>
2015-08-23 20:12:48 -0300

    remove assets

--- public/assets/app/student/timelines-a1923235fb4af09635c98b070a0989de.js
+++
@@ -1,1 +0,0 @@
-function set_timeline(e,n){var a=JSON.parse(window.calendar.replace(/&quot;/gi,'"')),o=JSON.parse(window.items.replace(/&quot;/gi,'"')),c=window.json;for(i in o)o[i].link&&(o[i].link=o[i].link.replace("academico/item","academico/timeline/"+e));header(c,o,a.half,function(){body(o),events(o)},n)}
--- public/assets/timeline/admin/events-9a56b4d77acbbdf68db05b789391b3c5.js
+++
@@ -1,4 +0,0 @@
-function setEvents(ctrlAngular){
-
-}
-;
