4767690ed1068e28c56a32d56376a09cab4906a6
Érico Dias <ericodias1@gmail.com>
2015-08-27 18:01:21 -0300

    solicitação de matrícula de TCC 2 como default

--- app/models/base_timeline.rb
+++ app/models/base_timeline.rb
@@ -118,15 +118,23 @@ class BaseTimeline < ActiveRecord::Base
   end
 
   def default_items_hash_tcc2(base_url)
-    # guardar em um arquivo
+    first_month = half == "1" ? 10 : 5
     [
+      {
+        title: 'Solicitação da matrícula de TCC 2',
+        _type: 'important',
+        description: 'O acadêmico deve solicitar a matrícula até 50 dias antes do término do semestre, via protocolo no DERAC.',
+        base_timeline_id: id,
+        link: '#',
+        date: Date.new(year, first_month, 1)
+      },
       {
         title: 'Entrega da monografia de TCC',
         _type: 'document',
         description: 'O acadêmico deve entregar 3 cópias encadernadas da monografia final ao professor responsável pelo TCC, para que seja encaminhada para avaliação dos professores que participarão da banca de TCC 2.',
         base_timeline_id: id,
         link: 'http://'+base_url+'/academico/item/{{id}}',
-        date: Date.new(year, 11, 1)
+        date: Date.new(year, first_month+1, 1)
       },
       {
         title: 'Entrega da ficha de autorização de defesa',
@@ -134,15 +142,15 @@ class BaseTimeline < ActiveRecord::Base
         description: 'Acompanhado da monografia, o acadêmico deve entregar a Ficha de Encaminhamento de TCC para Banca Avaliadora assinada pelo seu orientador.',
         base_timeline_id: id,
         link: 'http://'+base_url+'/academico/item/{{id}}',
-        date: Date.new(year, 11, 1)
+        date: Date.new(year, first_month+1, 1)
       },
-      {
+      {''
         title: 'Entrega da ficha de acompanhamento de atividades',
         _type: 'document',
         description: 'Acompanhado do projeto, o acadêmico deve entregar a Ficha de Acompanhamentos de Atividades de TCC assinada.',
         base_timeline_id: id,
         link: 'http://'+base_url+'/academico/item/{{id}}',
-        date: Date.new(year, 11, 1)
+        date: Date.new(year, first_month+1, 1)
       },
       {
         title: 'Defesa do TCC',
@@ -150,7 +158,7 @@ class BaseTimeline < ActiveRecord::Base
         description: 'O acadêmico deve realizar uma apresentação de no máximo 20 minutos para defender o seu trabalho de TCC. Esta apresentação, assim como a monografia, será avaliado por uma banca composta de 3 professores.',
         base_timeline_id: id,
         link: '#',
-        date: Date.new(year, 11, 10)
+        date: Date.new(year, first_month+1, 10)
       },
       {
         title: 'Entrega da monografia corrigida',
@@ -158,7 +166,7 @@ class BaseTimeline < ActiveRecord::Base
         description: 'Após as correções solicitadas pela banca o aluno deve entregar a versão final do TCC para o professor responsável juntamente com a ficha de encaminhamento da versão final do TCC, nas datas divulgadas em cada semestre.',
         base_timeline_id: id,
         link: 'http://'+base_url+'/academico/item/{{id}}',
-        date: Date.new(year, 11, 1)
+        date: Date.new(year, first_month+1, 1)
       },
       {
         title: 'Depósito do trabalho na biblioteca',
@@ -166,7 +174,7 @@ class BaseTimeline < ActiveRecord::Base
         description: 'Após as correções solicitadas pela banca o aluno deve entregar a versão final do TCC para o professor responsável juntamente com a ficha de encaminhamento da versão final do TCC, nas datas divulgadas em cada semestre.',
         base_timeline_id: id,
         link: 'http://'+base_url+'/academico/item/{{id}}',
-        date: Date.new(year, 11, 1)
+        date: Date.new(year, first_month+1, 1)
       }
     ]
   end
