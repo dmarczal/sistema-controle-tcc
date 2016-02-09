14325484bff0d008323b85c5a5e48698e0af8efe
Diego Marczal <dmarczal@gmail.com>
2016-02-09 15:00:58 -0200

    Added dropbox.yml link

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :deploy_via,      :remote_cache
 set :use_sudo,        false
 set :deploy_to, "/home/#{fetch(:user)}/apps/rails/#{fetch(:application)}"
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml, config/dropbox.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
